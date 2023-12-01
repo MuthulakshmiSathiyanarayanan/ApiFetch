8bfc001a75e80849bc389a85bb73c171b9b6379b
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2023-12-01 22:53:39 +0530

    api_fetch for hotCoffee,wine,beer and games with tryCache

--- src/App.js
+++ src/App.js
@@ -2,17 +2,73 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [data, setData] = useState(" ");
-  // console.log(data);
-  const getData = async () => {
-    const resp = await fetch("https://api.sampleapis.com/coffee/hot");
-    const json = await resp.json();
-    console.log(json);
-    setData(json);
+  const [hotcoffee, sethotcoffee] = useState([]);
+  const [wine, setwine] = useState(" ");
+  const [beer, setbeer] = useState(" ");
+  const [games, setgames] = useState("");
+
+  // fetching hotcoffee list
+  const gethotcoffee = async () => {
+    try {
+      const resp = await fetch("https://api.sampleapis.com/coffee/hothjjh");
+      const json = await resp.json();
+      console.log("-----hotcoffee-------json", json);
+      sethotcoffee(json);
+    } catch (err) {
+      alert("Something Error Happened, try after sometime");
+      console.log("Hot Cofee Api failed-------", err);
+    }
   };
+
+  //fetching wine details
+  const getwine = async () => {
+    const resp = await fetch("https://api.sampleapis.com/wines/reds");
+    const json = await resp.json();
+    console.log("-----wine-------json", json);
+    setwine(json);
+  };
+  // fetching beer prices
+  const getbeer = async () => {
+    const resp = await fetch("https://api.sampleapis.com/beers/ale");
+    const json = await resp.json();
+    console.log("-----beer-------json", json);
+    //beer = json;
+    setbeer(json);
+  };
+  // fetching game types
+  const getgames = async () => {
+    const resp = fetch("https://api.sampleapis.com/switch/games");
+    const json = await resp.json();
+    console.log("-----games-------json", json);
+    //games = json;
+    setgames(json);
+  };
+
   useEffect(() => {
-    getData();
+    // getwine();
+    // getbeer();
+    gethotcoffee();
+    // getgames();
   }, []);
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  console.log(
+    "-----------hotcoffee--------------",
+    hotcoffee.length,
+    typeof hotcoffee
+  );
+
+  return (
+    <pre>
+      {hotcoffee?.map((e) => (
+        <>
+          <span>{e.title}</span>
+          <br />
+        </>
+      ))}
+      {/* {JSON.stringify(hotcoffee, null, 2)}
+      {JSON.stringify(wine, null, 2)}
+      {JSON.stringify(beer, null, 2)}
+      {JSON.stringify(games, null, 2)} */}
+    </pre>
+  );
 }
