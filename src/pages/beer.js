b1038ce1c36aa5e1eb4fa5078ad48d4e94534426
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2023-12-10 21:34:33 +0530

    beer search completed

--- src/pages/beer.js
+++ src/pages/beer.js
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../App.css";
 import BeerComponent from "../components/beerComponent";
 import api from "../common/json/api.json";
 
 export default function BeerApp() {
   const [beer, setBeer] = useState([]);
+  const [originalBeer, setOriginalBeer] = useState([]);
+  const [searchBoxValue, setSearchBoxValue] = useState();
+  const inputRef = useRef(null);
 
   // fetching beer prices
   const getBeer = async () => {
@@ -13,7 +16,7 @@ export default function BeerApp() {
       const json = await resp.json();
       console.log("-----beer-------json", json);
       //beer = json;
-      setBeer(json);
+      setOriginalBeer(json);
     } catch (err) {
       console.log("Beer Api failed-------", err);
     }
@@ -23,11 +26,27 @@ export default function BeerApp() {
     getBeer();
   }, []);
 
+  //handle search
+  const handleSearch = () => {
+    const srcValue = inputRef?.current?.value;
+    const searchedValue = originalBeer?.filter(
+      (element) =>
+        element?.name?.toUpperCase()?.indexOf(srcValue?.toUpperCase()) !== -1
+    );
+    setBeer(srcValue ? searchedValue : originalBeer);
+  };
+
   return (
-    <div className="BeverageContainer">
-      {beer?.map((e) => (
-        <BeerComponent name={e.name} price={e.price} image={e.image} />
-      ))}
+    <div>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleSearch}>Search</button>
+      <br />
+      <span>This is the search result for: {searchBoxValue}</span>
+      <div className="BeverageContainer">
+        {beer?.map((e) => (
+          <BeerComponent name={e.name} price={e.price} image={e.image} />
+        ))}
+      </div>
     </div>
   );
 }
