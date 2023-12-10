08beae976670c2156551476985b3a1aefdce9c4a
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2023-12-11 00:32:57 +0530

    context creation init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
-import React from "react";
+import React, { createContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./pages/header";
 
+export const SearchKeyContext = createContext();
+
 export default function App() {
+  const globalSearchState = useState("");
+
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    <SearchKeyContext.Provider value={globalSearchState}>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+    </SearchKeyContext.Provider>
   );
 }
--- src/pages/hotCoffee.js
+++ src/pages/hotCoffee.js
@@ -1,23 +1,28 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import "../App.css";
-//import App from "../App";
+import { SearchKeyContext } from "../App";
 import HotCoffeeComponent from "../components/hotCoffeeComponent";
 import api from "../common/json/api.json";
 
 export default function HotCoffeeApp() {
   const [hotCoffee, setHotCoffee] = useState([]);
   const [originalHotCoffee, setOriginalHotCoffee] = useState([]);
-  const [searchBoxValue, setSearchBoxValue] = useState("...");
-  const inputRef = useRef(null);
+  // const [searchBoxValue, setSearchBoxValue] = useState("...");
+  // const inputRef = useRef(null);
+  const [srcCtxVal] = useContext(SearchKeyContext);
+  console.log("---------srcCtxVal-------------------", srcCtxVal);
 
   // fetching hotCoffee list
   const getHotCoffee = async () => {
     try {
       const apiResp_hotcoffee = await fetch(api[0]);
       const json_hotCoffeeFetch = await apiResp_hotcoffee.json();
-      console.log("-----hotCoffee-------json", json_hotCoffeeFetch);
+      // console.log("-----hotCoffee-------json", json_hotCoffeeFetch);
       setHotCoffee(json_hotCoffeeFetch);
       setOriginalHotCoffee(json_hotCoffeeFetch);
+
+      // Trial test search context
+      handleContextSearch(json_hotCoffeeFetch);
     } catch (err) {
       setHotCoffee({ sorry: "Some Server Errror" });
       alert("Something Error Happened, try after sometime");
@@ -29,8 +34,25 @@ export default function HotCoffeeApp() {
     getHotCoffee();
   }, []);
 
+  useEffect(() => {
+    handleContextSearch();
+  }, [srcCtxVal]);
+
+  const handleContextSearch = (defaultValue = originalHotCoffee) => {
+    const searchedValue = defaultValue?.filter(
+      (element) =>
+        element?.title?.toUpperCase()?.indexOf(srcCtxVal?.toUpperCase()) !== -1
+    );
+    console.log(
+      "--------searchedValuesearchedValue-------",
+      searchedValue,
+      defaultValue
+    );
+    setHotCoffee(srcCtxVal ? searchedValue : defaultValue);
+  };
+
   //displaying the searched searchBox contents
-  const handleSearch = () => {
+  /* const handleSearch = () => {
     const srcValue = inputRef?.current?.value;
     console.log("--srcvalue--", srcValue);
     //Filter operation
@@ -43,27 +65,25 @@ export default function HotCoffeeApp() {
     console.log("---basic check--", setHotCoffee);
     console.log("--checking the setState--", hotCoffee);
     setSearchBoxValue(srcValue);
-  };
+  }; */
 
   //console logs for hotCoffee and original hot coffee changes
-  console.log(
-    "-----------hotCoffee--------------",
-    hotCoffee.length,
-    typeof hotCoffee
-  );
-  console.log("----hotcoffee after search---", hotCoffee);
-  console.log("--original hotcoffee after search---", originalHotCoffee);
+  // console.log(
+  //   "-----------hotCoffee--------------",
+  //   hotCoffee.length,
+  //   typeof hotCoffee
+  // );
+  // console.log("----hotcoffee after search---", hotCoffee);
+  // console.log("--original hotcoffee after search---", originalHotCoffee);
 
   //return statements start
   return (
     <div>
-      <input type="text" ref={inputRef} />
+      {/* <input type="text" ref={inputRef} /> */}
       {/* uncontrolled component- to make it controlled can call value=
       {searchBoxValue} in input type*/}
-      <button onClick={handleSearch}>Search</button>
-      <br />
-      <span>This is the search result for: {searchBoxValue}</span>
-      <br />
+      {/* <button onClick={handleSearch}>Search</button>
+      <br /> */}
       <div className="BeverageContainer">
         {hotCoffee?.map((e) => (
           <HotCoffeeComponent {...e} />
